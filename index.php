637e1aae3ee72aeabd4b83d7e5a32270d889b92a
TakumiNakahara <XXXX@hogehoge.com>
2023-07-24 15:36:47 -0400

    Built a environment

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>環境構築</title>
+</head>
+<body>
+    <p><?php print ("Hello World!");?></p>
+    <h1>環境構築完了</h1>
+    <script src="script.js"></script>
+</body>
+</html>
